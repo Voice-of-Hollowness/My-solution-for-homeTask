14ea1f593f96af82b1ce340c1b3b1af9d27f4000
iOne Miroshnichenko <myroshnichenkoivan89@gmail.com>
2023-04-03 18:53:53 +0300

    before AI takeover our project

--- sources/docs/intro.md
+++ sources/docs/intro.md
@@ -4,6 +4,7 @@ sidebar_position: 1
 
 # Tutorial Intro
 
+Доброго дня Я іван і ось моя сторінка
 Let's discover **Docusaurus in less than 5 minutes**.
 
 ## Getting Started
--- sources/src/pages/index.js
+++ sources/src/pages/index.js
@@ -12,13 +12,18 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <div class="slideshow-container">
+        </div>
+
+          
+
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            IOnePage Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -26,12 +31,15 @@ function HomepageHeader() {
   );
 }
 
+
+
+//here well mbe new code 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Створю сторінки швидко та просто<head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
